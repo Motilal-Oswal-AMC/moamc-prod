46df92e2f3a60af55fb373ad0f5ade79ef4606c8
Yoge93 <yogesh.walwalkar@deptagency.com>
2025-11-27 15:03:48 +0530

    header changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -198,6 +198,7 @@ async function loadEager(doc) {
     decorateMain(main);
     buildAutoBlocks(main);
     decorateAutoBlock(doc);
+    await loadHeader(doc.querySelector('header'));
     document.body.classList.add('appear');
     await loadSection(main.querySelector('.section'), waitForFirstImage);
   }
@@ -244,7 +245,6 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
