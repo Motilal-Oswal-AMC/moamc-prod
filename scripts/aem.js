7a7745af2aee95bb625821e5d90ccfcc7ee16795
Yoge93 <yogesh.walwalkar@deptagency.com>
2025-11-28 17:31:03 +0530

    code added

--- scripts/aem.js
+++ scripts/aem.js
@@ -9,6 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+console.log("code prod");
 
 /* eslint-env browser */
 function sampleRUM(checkpoint, data) {
